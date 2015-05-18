2d0b13bd23bead27feb6c42131355d39f84c08cb
Portia Plante <plantep@winthrop.edu>
2015-05-18 13:02:49 -0400

    ds

--- js/script.js
+++ js/script.js
@@ -10,16 +10,17 @@ $(function(){
 //shows/hides details when the carrot is clicked
 function showDetails()
 {	
-	var id = $(this).attr("href");
-	$(id).slideToggle();
-	$(this).find("#carrot").toggleClass("fa fa-chevron-down").toggleClass("fa fa-chevron-up");
-
-	//scrolls the content to the top
+	//move to top
 	var parent = $(this).parent();
-	 
+	
 	$('html, body').animate({
     	scrollTop: parent.offset().top -80},
         500);
+	
+	//toggle	
+	var id = $(this).attr("href");
+	$(id).slideToggle();
+	$(this).find("#carrot").toggleClass("fa fa-chevron-down").toggleClass("fa fa-chevron-up");
 
 	return false;
 }
