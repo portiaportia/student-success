0ece0f5664a59fe8af410616b074c782e24ef49e
Portia Plante <plantep@winthrop.edu>
2015-05-19 18:34:29 -0400

    ds

--- js/events.js
+++ js/events.js
@@ -34,11 +34,11 @@ function showEvent(i, item)
 	var dateDisplay = item.date + " " + item.time;
 	var eventDetails = "<h4>" + item.description + "</h4>";
 	eventDetails += "<ul><li>Location: " + item.location + "</li>";
-	eventDetails += "<li>Time: " + dateDisplay + "</li>";
-	eventDetails += "<li>Duration: " + item.duration + "</li>";
+	eventDetails += "<li>Time: " + dateDisplay + "</li></ul>";
+	/*eventDetails += "<li>Duration: " + item.duration + "</li>";
 	eventDetails += "<li>Contact: <a href='tel:+"  + item.contact + "'>" + item.contact + "</a></li>";
 	eventDetails += "<li>Price: " + item.admission + "</li>";	
-	eventDetails += "<li>GLI : " + item.gli + "</li></ul>";
+	eventDetails += "<li>GLI : " + item.gli + "</li></ul>";*/
 	
 	
 	$("#event-list").append("<li class='event-details'>" + eventDetails + "</li>");
