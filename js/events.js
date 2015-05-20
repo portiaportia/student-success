0a9d3a47bcf9ca358508317c4d318299bce1aec5
Portia Plante <plantep@winthrop.edu>
2015-05-20 13:35:54 -0400

    dsfa

--- js/events.js
+++ js/events.js
@@ -9,7 +9,8 @@ function loadEvents()
 	
 	loader = $("<div id='loader'><img src='images/loading.gif' alt='loading...'></div>");	
 	loader.appendTo("#event-list").hide();
-
+	loader.addClass("load");
+	
 	var today = new Date();
 	var startDate = convertDate(today);
 	var endDate = convertDate(today + 30);
@@ -17,14 +18,14 @@ function loadEvents()
 	var url = "http://apps.winthrop.edu/API/CulturalEvents/events?startDate=" + startDate + "&endDate=" + endDate;
 	url = "http://apps.winthrop.edu/API/CulturalEvents/events?startDate=4/10/2015&endDate=5/10/2015";
 	
-	//url = "js/events-json.js";
+	url = "js/events-json.js";
 	
 	$.ajax({
 				type:"GET",
 				dataType:"json",
 				url:url,
-				beforeSend:function(){loader.show();},
-				complete:function(){loader.hide();},
+				beforeSend:function(){loader.removeClass("hide");},
+				complete:function(){loader.addClass("show");},
 				success:parseEvents
 			});			
 }
@@ -46,19 +47,17 @@ function parseEvents(data)
 function showEvent(i, item)
 {
 	var eventDate = new Date(item.EventDate);
-	var dateDisplay = "<ul><li>" + convertDate(eventDate) + "</li>";
-	dateDisplay+= "<li>" + item.StartTime + " - " + item.EndTime + "</li>";
+	
 	
 	eventDetails += dateDisplay;
 	var eventDetails = "<h4>" + item.Title + "</h4>";
-	eventDetails += "<ul><li>" + item.Description + "</li>";
-	eventDetails += "<li><strong>Location: </strong>" + item.Location + "</li>";
-	eventDetails += "<li><strong>Contact:</strong> " + item.ContactPersonName + " <a href='tel:+" + item.ContactPersonPhoneNumber + "'>" + item.ContactPersonPhoneNumber +"</a></li>";
-
-	if(item.IsGLI) {
-		dateDisplay +=	"<li>GLI Event</li>";
-	}
+	//eventDetails += "<ul><li>" + item.Description + "</li>";
+	eventDetails += "<ul><li><strong>Time: </strong>" + convertDate(eventDate);
+	eventDetails += " (" + item.StartTime + " - " + item.EndTime + ")</li>";
 	
+	eventDetails += "<li><strong>Location: </strong>" + item.Location + "</li>";
+	eventDetails += "<li><strong>Contact:</strong> <a href='tel:+" + item.ContactPersonPhoneNumber + "'>" + item.ContactPersonPhoneNumber +"</a></li>";
+
 	if(item.PriceDescription != "N/A")
 	{
 		eventDetails += "<li><strong>Price:</strong> " + item.PriceDescription + "</li>";
@@ -66,6 +65,10 @@ function showEvent(i, item)
 	else
 	{
 		eventDetails += "<li><strong>Price:</strong> free</li>";
+	}
+	
+	if(item.IsGLI) {
+		eventDetails +=	"<li>GLI Event</li>";
 	}
 	
 	eventDetails += "</ul>";
@@ -77,5 +80,5 @@ function showEvent(i, item)
 	eventDetails += "<li>GLI : " + item.gli + "</li></ul>";*/
 	
 	
-	$("#event-list").append("<li><div class='event-date'>" + dateDisplay + "</div><div class='event-details'>" + eventDetails + "</div></li>");
+	$("#event-list").append("<li><div class='event-details'>" + eventDetails + "</div></li>");
 }
