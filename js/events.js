293b5ddc3f165c953e291b493a2a35b1ecde46b4
Portia Plante <plantep@winthrop.edu>
2015-05-25 21:40:36 -0400

    dsf

--- js/events.js
+++ js/events.js
@@ -99,5 +99,5 @@ function showEvent(i, item)
 	$("#event-list").append("<li><div class='event-details'>" + eventDetails + "</div></li>");
 	
 	//resize the page to account for header now that events are loaded
-	$("#events").height($("#events").height() +80);
+	//$("#events").height($("#events").height() +80);
 }
