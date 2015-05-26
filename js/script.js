8918f25fee6aba054716575e8149bc7ca84e2d8c
Portia Plante <plantep@winthrop.edu>
2015-05-26 13:29:01 -0400

    sdf

--- js/script.js
+++ js/script.js
@@ -39,12 +39,12 @@ function showDetails()
 }
 
 //resize all pages to account for the header when they show
-$(document).on("pageshow","#calendar",function(){ 
+$(document).on("pageshow",".page",function(){ 
   resizeCal();
 });
 			
 function resizeCal()
 {
-	//$(".page:visible").not("#events").not("#map").height($(".page:visible").height() +80);
-	$("#calendar").height($("#calendar").height() +80);
+	$(".page:visible").not("#events").not("#map").height($(".page:visible").height() +80);
+	//$("#calendar").height($("#calendar").height() +80);
 }
